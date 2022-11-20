e654a8a7a3e61d7f4e95ca05abdf06038cf325e0
Jeziel Almeida <jeziel.almeida16@gmail.com>
2022-11-19 23:28:29 -0300

    Informações adicionadas

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,8 @@
 import Head from "next/head";
 import { MdLightMode, MdDarkMode } from "react-icons/md"
-import { AiFillTwitterCircle, AiFillLinkedin, AiFillYoutube } from "react-icons/ai";
+import { AiFillTwitterCircle, AiFillLinkedin, AiFillGithub } from "react-icons/ai";
 import Image from "next/image";
-import deved from "../public/dev-ed-wave.png";
+import deved from "../public/devjeziel.png";
 import design from "../public/design.png";
 import code from "../public/code.png";
 import consulting from "../public/consulting.png";
@@ -29,7 +29,7 @@ export default function Home() {
 
       <main className="bg-white px-10 md:px-20 lg:px-40 dark:bg-gray-900">
         <section className="min-h-screen">
-          <nav className="py-10 mb-12 flex justify-between dark:text-white">
+          <nav className="py-10 mb-4 flex justify-between dark:text-white">
             <h1 className="text-xl font-burtons">Jeziel Almeida</h1>
             <ul className="flex items-center">
               {!lightIcon 
@@ -49,28 +49,28 @@ export default function Home() {
                   </li>
               }
               <li>
-                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8" href="#">Resume</a>
+                <a className="bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8" href="#">Currículo</a>
               </li>
             </ul>
           </nav>
 
           <div className="text-center p-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium md:text-6xl dark:text-teal-400" id="AnimatedTitle">Jeziel Almeida</h2>
-            <h3 className="text-2xl py-2 md:text-3xl dark:text-white">Developer and Designer</h3>
-            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-gray-200">Freelancer providing services for programming and design content needs.</p>
+            <h3 className="text-2xl py-2 md:text-1xl dark:text-white">Developer and Designer</h3>
+            <p className="text-md py-5 leading-8 text-gray-800 md:text-xl max-w-lg mx-auto dark:text-gray-200">Estudando programação e, atualmente, no 4º período de Análise e Desenvolvimento de Sistemas.</p>
           </div>
           <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
-            <AiFillTwitterCircle className="cursor-pointer" />
-            <AiFillLinkedin className="cursor-pointer" />
-            <AiFillYoutube className="cursor-pointer" />
+            <a href="https://twitter.com/barrosjeziel"><AiFillTwitterCircle className="cursor-pointer hover:text-teal-600" /></a>
+            <a href="https://www.linkedin.com/in/jeziel-almeida-352252147"><AiFillLinkedin className="cursor-pointer hover:text-teal-600" /></a>
+            <a href="https://github.com/jeziel-almeida"><AiFillGithub className="cursor-pointer hover:text-teal-600" /></a>
           </div>
 
           <div className="relative mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 mt-20 mb-20 overflow-hidden md:h-96 md:w-96">
             <Image src={deved} layout="fill" objectFit="cover" alt="dev" />
           </div>
         </section>
-
-        {/* SECTION */}
+	
+	{/*
         <section>
           <div>
             <h3 className="text-3xl py-1 dark:text-white">Services I offer</h3>
@@ -128,23 +128,18 @@ export default function Home() {
             </div>
           </div>
         </section>
-
+	*/}
         {/* PORTFOLIO */}
         <section>
           <div>
-            <h3 className="text-3xl py-1 dark:text-white">Portfolio</h3>
+            <h3 className="text-3xl py-1 dark:text-white font-semibold">Portfolio</h3>
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I"ve done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
+              Aqui estão alguns projetos que realizei ao longo do meu aprendizado de programação front-end. São projetos que utilizam tecnologias como Javascript, HTML, CSS, Canvas, React, Next.js, Vite.js, entre outras.
             </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
-            </p>
+	    <p className="text-md leading-8 text-gray-800 dark:text-gray-200">
+	      Ao clicar na imagem, você será redirecionado ao link do repositório do projeto no github.
+	    </p>
+            
           </div>
 
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
